2cc24d3c956b92d6fe1cce94c22683e165bbe76b
Wasima230Sultana <wasimasultana230@gmail.com>
2025-09-25 20:41:53 +0600

    Add assets

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import navIng from './assets/logo.png'
+import dollarIng from './assets/dollar.png'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div class="navbar max-w-[1200px] mx-auto">
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl">
+      <img className='w-[60px] h-[60px]' src={navIng} alt="" />
+    </a>
+  </div>
+  <div class="flex items-center ">
+    <span className='mr-1'>0</span>
+    <span className='mr-1'>Coin</span>
+    <img src={dollarIng} alt="" />
+  </div>
+</div>
+      
     </>
   )
 }
